4f77b2aa8befeb97f0dfdbd21351f9b792cbed38
dimchik_st@list.ru <dimchik_st@list.ru>
2022-02-06 17:47:36 +0300

    import modules

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,14 +6,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WithCredentialsInterceptor } from './modules/app-root/interceptors/with-credentials.interceptor';
 import { AddHeadersInterceptor } from './modules/app-root/interceptors/add-headers.interceptor';
+import { TodosModule } from './modules/todos/todos.module';
+import { PageNotFoundComponent } from './modules/app-root/pages/page-not-found/page-not-found.component';
+import { UserModule } from './modules/user/user.module';
 
 @NgModule({
   declarations: [
     AppComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    TodosModule,
+    UserModule,
     AppRoutingModule,
   ],
   providers: [
