6be00c944aadfb952789d2f4c443ebc2761135e6
dimchik_st@list.ru <dimchik_st@list.ru>
2022-02-06 23:46:30 +0300

    fetch csrf cookie on init

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   template: `<router-outlet></router-outlet>`,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get(environment.apiUrl + '/csrf-cookie').subscribe();
+  }
 }
