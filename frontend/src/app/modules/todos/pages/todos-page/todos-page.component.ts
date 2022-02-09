dd4dad0d7aff17fecf880ff86b8c208ef5a08739
dimchik_st@list.ru <dimchik_st@list.ru>
2022-02-09 13:53:10 +0300

    clean up code

--- frontend/src/app/modules/todos/pages/todos-page/todos-page.component.ts
+++ frontend/src/app/modules/todos/pages/todos-page/todos-page.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-todos-page',
   templateUrl: './todos-page.component.html',
-  styleUrls: ['./todos-page.component.scss']
+  styleUrls: ['./todos-page.component.scss'],
 })
-export class TodosPageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class TodosPageComponent { }
