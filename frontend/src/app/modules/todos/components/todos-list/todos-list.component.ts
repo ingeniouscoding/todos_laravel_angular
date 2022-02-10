dc039551151912d94cc83f7e710d2cf432f293c6
dimchik_st@list.ru <dimchik_st@list.ru>
2022-02-10 21:47:05 +0300

    add leave enter animations

--- frontend/src/app/modules/todos/components/todos-list/todos-list.component.ts
+++ frontend/src/app/modules/todos/components/todos-list/todos-list.component.ts
@@ -1,3 +1,4 @@
+import { animate, keyframes, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { map } from 'rxjs';
 
@@ -8,6 +9,20 @@ import { Todo } from '../../types/todo.interface';
   selector: 'app-todos-list',
   templateUrl: './todos-list.component.html',
   styleUrls: ['./todos-list.component.scss'],
+  animations: [
+    trigger('inOutBlock', [
+      transition(':enter', [
+        style({ transform: 'translate(-200px, -100px) scaleY(0)', opacity: 0 }),
+        animate('0.3s', style({ transform: 'translate(0) scaleY(100%)', opacity: 1 })),
+      ]),
+      transition(':leave', [
+        animate('0.3s', keyframes([
+          style({ transform: 'translateX(70%)', opacity: 0.2, offset: 0.7 }),
+          style({ transform: 'translateX(100%)', height: 0, margin: 0, offset: 1 }),
+        ])),
+      ]),
+    ]),
+  ],
 })
 export class TodosListComponent implements OnInit {
   public todos$ = this.todosService.todos$
